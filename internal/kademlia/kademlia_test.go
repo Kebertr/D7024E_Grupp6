6b7bc219de9ae659dffeb0c8f001260b3a82476f
Alex Burman <alebur-2@student.ltu.se>
2026-09-23 14:05:49 +0200

    testing lookupData

--- internal/kademlia/kademlia_test.go
+++ internal/kademlia/kademlia_test.go
@@ -106,6 +106,53 @@ func TestStoreStoresDataLocally(t *testing.T) {
 	}
 }
 
+func TestLookupDataFindsRemoteValue(t *testing.T) {
+	mock := NewMockNetwork()
+	node1 := NewContact(
+		NewKademliaID("0000000000000000000000000000000000000000000000000000000000000001"),
+		"node1",
+	)
+	node2 := NewContact(
+		NewKademliaID("0000000000000000000000000000000000000000000000000000000000000002"),
+		"node2",
+	)
+
+	network1, err := initNetwork(mock, node1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	network2, err := initNetwork(mock, node2)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	data := []byte("value")
+	targetID := hashData(data)
+	kademlia1 := &Kademlia{
+		Contact:      node1,
+		RoutingTable: NewRoutingTable(node1),
+		Network:      network1,
+	}
+	kademlia1.RoutingTable.AddContact(node2)
+	kademlia2 := &Kademlia{
+		Contact:      node2,
+		RoutingTable: NewRoutingTable(node2),
+		Network:      network2,
+		Data:         map[string][]byte{targetID.String(): data},
+	}
+
+	network1.ServerListen(kademlia1)
+	network2.ServerListen(kademlia2)
+
+	result, err := kademlia1.LookupData(targetID.String())
+	if err != nil {
+		t.Fatalf("LookupData failed: %v", err)
+	}
+	if string(result) != string(data) {
+		t.Fatalf("expected value %q, got %q", data, result)
+	}
+}
+
 func TestNextUnqueried(t *testing.T) {
 	node1 := NewContact(NewKademliaID(
 		"0000000000000000000000000000000000000000000000000000000000000001",
